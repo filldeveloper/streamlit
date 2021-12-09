63a5ae44c14e01edc30c4641e26a76ec9a1d4850
filldeveloper <felipebarreto.silva@hotmail.com>
2021-12-09 15:07:41 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+# import mymodel as m
+
+st.title("Felipe Barreto da Silva")
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write("""
+This is our first Web App.
+Enjoy it!
+""")
+st.button('Submit')
+# st.write(m.run(window=15))
