89bdd363ee942aabd33ed6d02715c01634a703c2
filldeveloper <felipebarreto.silva@hotmail.com>
2021-12-09 16:46:28 -0300

    first edition

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import streamlit as st
+import pandas as pd
 # import mymodel as m
 
+data = {
+    'Coluna 1': [1, 5, 9, 10, 11],
+    'Coluna 2': [10, 20, 50, 60, 70]
+}
+df = pd.DataFrame(data)
 st.title("Felipe Barreto da Silva")
 st.subheader('Introducing Streamlit in Automate Everything with Python')
 st.write("""
 This is our first Web App.
 Enjoy it!
 """)
+st.write(df)
 st.button('Submit')
 # st.write(m.run(window=15))
